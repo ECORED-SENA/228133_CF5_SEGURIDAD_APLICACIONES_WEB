78b8a9b0b73b3ef7707c28e1009633837bf0cb28
MarciaSolano <99497342+MarciaSolano@users.noreply.github.com>
2022-09-21 15:36:35 -0500

    Update global

--- src/config/global.js
+++ src/config/global.js
@@ -153,32 +153,220 @@ export default {
   },
   complementario: [
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      link: 'https://www.google.com/',
+      tema: 'Definición de Métricas de Seguridad en Aplicaciones Web',
+      referencia: 'OWASP Security Qualitative Metrics. (s/f). Owasp.Org.',
+      tipo: 'Página web',
+      link:
+        'https://owasp.org/www-project-security-qualitative-metrics/SECURITY-QUALITATIVE-METRICS',
     },
     {
-      tema: '',
-      referencia: '',
-      tipo: '',
-      descarga: '/downloads/prueba.pdf',
+      tema:
+        'El marco de conocimiento de seguridad (Security Knowledge Framework)',
+      referencia:
+        'wstg: The Web Security Testing Guide is a comprehensive Open Source guide to testing the security of web applications and web services. (s/f).',
+      tipo: 'Repositorio',
+      link: 'https://github.com/OWASP/wstg',
+    },
+    {
+      tema:
+        'El marco de conocimiento de seguridad (Security Knowledge Framework)',
+      referencia:
+        'Pereira, C. P. T.-D. [DavidPereira]. (2021, marzo 4). Curso de Pentesting a Aplicaciones WEB (Video 1) OWASP Juice Shop. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=6eAJ1dinee0&t=690s',
+    },
+    {
+      tema:
+        'El marco de conocimiento de seguridad (Security Knowledge Framework)',
+      referencia:
+        'Tech, T. [TaggartTech]. (2021, septiembre 8). #AttackOnTuesday | OWASP ZAP/Juice Shop. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=4CNWTkAbT4o&t=801s',
+    },
+    {
+      tema:
+        'UskoKruM [UskoKruM2010]. (2021, agosto 19). Cómo Descargar e Instalar Node.js en Windows 10 | Explicación Sencilla. Youtube.',
+      referencia:
+        'UskoKruM [UskoKruM2010]. (2021, agosto 19). Cómo Descargar e Instalar Node.js en Windows 10 | Explicación Sencilla. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=ipmhBYqIP44&t=4s',
+    },
+    {
+      tema:
+        'Eber, D. [donEber]. (2021, junio 17). Instalación de Git en Windows paso a paso | [2021| 2022]. Youtube.',
+      referencia:
+        'Eber, D. [donEber]. (2021, junio 17). Instalación de Git en Windows paso a paso | [2021| 2022]. Youtube.',
+      tipo: 'Video',
+      link: 'https://www.youtube.com/watch?v=cYLapo1FFmA&t=2s',
     },
   ],
   glosario: [
     {
-      termino: '',
-      significado: '',
+      termino: 'API Endpoints',
+      significado:
+        'Son las URLs de un API o un backend que responden a una petición.',
     },
     {
-      termino: '<em></em>',
-      significado: '',
+      termino: 'API REST',
+      significado:
+        'Es una arquitectura que permite la interacción con los servicios web de RESTful.',
+    },
+    {
+      termino: 'Aplicaciones POS',
+      significado:
+        'Son un tipo de programas destinados a sistematizar el control de ventas e inventarios del punto físico de un negocio.',
+    },
+    {
+      termino: 'Ataques Oportunistas',
+      significado:
+        'Es un tipo de ataque a un sistema software que aprovecha algún punto vulnerable que no ha sido identificado previamente.',
+    },
+    {
+      termino: 'Bearer Token',
+      significado:
+        'Es un token de acceso seguro que contiene las credenciales de seguridad para la gestión de la sesión e identificación del usuario.',
+    },
+    {
+      termino: 'Bombas lógicas de tiempo',
+      significado:
+        'Es un ataque malicioso programado a una fecha precisa a los sistemas.',
+    },
+    {
+      termino: 'Cámara de Compensación Automatizada (ACH)',
+      significado:
+        'Es un tipo de tecnología que permite procesar transacciones entre bancos y otras instituciones financieras.',
+    },
+    {
+      termino: 'Indicadores Claves de Rendimiento (KPI)',
+      significado:
+        'Es una medida del nivel de rendimiento o desempeño de un proceso.',
+    },
+    {
+      termino: 'Ingeniería social',
+      significado:
+        'Es una práctica ilegal que consiste en obtener información privada o confidencial a través de la manipulación de las personas y sus cuentas de usuario.',
+    },
+    {
+      termino: 'WYSIWYG',
+      significado:
+        '<b>Lo que ves es lo que tienes <em>(What You See Is What You Get )</b></em>, esto se aplica a los procesadores de texto y otros editores de texto con formato que permiten escribir un documento mostrando directamente el resultado final.',
     },
   ],
   referencias: [
     {
-      referencia: '',
+      referencia:
+        'Eber, D. [donEber]. (2021, junio 17). Instalación de Git en Windows paso a paso | [2021| 2022]. Youtube.',
+      link: 'https://www.youtube.com/watch?v=cYLapo1FFmA',
+    },
+    {
+      referencia:
+        'G. V. Ríos, John A. Bohada e I. A. Delgado, “Gestión de seguridad de la información en las organizaciones”, en Investigación e Innovación en Ingeniería del Software. Medellín: Tecnológico de Antioquia, vol. 2, 2018, pp. 111-121.',
       link: '',
+    },
+    {
+      referencia:
+        'Git. [En línea] 17 de mayo de 2022. [Citado el: 17 de mayo de 2022.]',
+      link: 'https://git-scm.com/download/win',
+    },
+    {
+      referencia:
+        'M. Seyyar, F. Çatak and E. Gül, “Detection of attack-targeted scans from the Apache HTTP Server access logs”,  Appl. Comput. Inf., vol. 14, N.º 1, pp. 28-36, 2018.',
+      link: '',
+    },
+    {
+      referencia:
+        'NodeJs. [En línea] 17 de mayo de 2022. [Citado el: 17 de mayo de 2022.]',
+      link: 'https://nodejs.org/es/download/',
+    },
+    {
+      referencia:
+        'Pereira, C. P. T.-D. [DavidPereira]. (2021, marzo 4). Curso de Pentesting a Aplicaciones WEB (Video 1) OWASP Juice Shop. Youtube.',
+      link: 'https://www.youtube.com/watch?v=6eAJ1dinee0',
+    },
+    {
+      referencia:
+        'R. Rodriguez, H. Alberto. “The Concept of Corporate Strategy. Innovar: Revista de Ciencias Administrativas y Sociales”. 19, 2009.',
+      link: '',
+    },
+    {
+      referencia:
+        'S. A. Pineda, John A. Bohada y M. L. Pineda, “Ingeniería Social en Instituciones de Educación Superior”, Revista Colombiana de Tecnologías de Avanzada, vol. 2, N.º 32, pp. 52-61, 2018.',
+      link: '',
+    },
+    {
+      referencia:
+        'Security Knowledge Framework. [En línea] 15 de mayo de 2022. [Citado el: 15 de mayo de 2022.]',
+      link: 'https://owasp.org/www-project-security-knowledge-framework/',
+    },
+    {
+      referencia:
+        'OWASP. 2021. [En línea] 11 de mayo de 2022. [Citado el: 11 de mayo de 2022.]',
+      link: 'https://owasp.org/',
+    },
+    {
+      referencia:
+        'OWASP. Top Ten. [En línea] 10 de mayo de 2022. [Citado el: 10 de mayo de 2022.]',
+      link: 'https://owasp.org/www-project-top-ten/',
+    },
+    {
+      referencia:
+        'OWASP Zed Attack Proxy (ZAP). [En línea] 15 de mayo de 2022. [Citado el: 15 de mayo de 2022.]',
+      link: 'https://owasp.org/www-project-zap/',
+    },
+    {
+      referencia:
+        'OWASP Juice Shop. [En línea] 15 de mayo de 2022. [Citado el: 15 de mayo de 2022.]',
+      link: 'https://owasp.org/www-project-juice-shop/',
+    },
+    {
+      referencia:
+        'UskoKruM [UskoKruM2010]. (2021, agosto 19). Cómo Descargar e Instalar Node.js en Windows 10 | Explicación Sencilla . Youtube.',
+      link: 'https://www.youtube.com/watch?v=ipmhBYqIP44',
+    },
+    {
+      referencia:
+        'V. Mouli and K. P. Jevitha, “Web Services Attacks and Security- ASystematic Literature Review”,  Procedia Comp. Sci., vol. 93, pp. 870-877, 2016.',
+      link: '',
+    },
+    {
+      referencia:
+        'wstg: The Web Security Testing Guide is a comprehensive Open Source guide to testing the security of web applications and web services. (s/f).',
+      link: '',
+    },
+    {
+      referencia:
+        'Pries, R., et al. Traffic Measurement and Analysis of a Broadband Wireless Internet Access. In Vehicular Technology Conference, 2009. VTC Spring 2009. IEEE 69th. 2009.',
+      link: '',
+    },
+    {
+      referencia:
+        'Xinyu, X. and S. Mishra. Where is the tight link in a home wireless broadband environment? in Modeling, Analysis & Simulation of Computer and Telecommunication Systems, 2009. MASCOTS 09. IEEE International Symposium on. 2009.',
+      link: '',
+    },
+    {
+      referencia:
+        'Dabir, A. and A. Matrawy. Bottleneck Analysis of Traffic Monitoring using Wireshark. in Innovations in Information Technology, 2007. IIT 07. 4th International Conference on. 2007.',
+      link: '',
+    },
+    {
+      referencia:
+        'Shaoqiang, W., X. DongSheng, and Y. ShiLiang. Analysis and application of Wireshark in TCP/IP protocol teaching. in E-Health Networking, Digital Ecosystems and Technologies (EDT), 2010 International Conference on. 2010.',
+      link: '',
+    },
+    {
+      referencia:
+        'Francesco Fusco, L.D., Joseph Gasparakis. Towards Monitoring Programmability in the Future Internet: challenges and solutions. 2010.',
+      link: '',
+    },
+    {
+      referencia:
+        'Microsoft. Microsoft Message Analyzer Operating Guide. [En línea] 21 de junio de 2022. [Citado el: 21 de junio de 2022.]',
+      link: 'https://technet.microsoft.com/enus/library/jj649776.aspx',
+    },
+    {
+      referencia:
+        'PRTG Network Monitor. [En línea] 21 de junio de 2022. [Citado el: 21 de junio de 2022.]:',
+      link: 'https://www.paessler.com/manuals/prtg',
     },
   ],
   creditos: {
@@ -196,6 +384,28 @@ export default {
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
+      {
+        nombre: 'Danny Alejandro Solano',
+        cargo: 'Experto temático',
+        centro:
+          'Centro de teleinformática y Producción Industrial - Regional Cauca',
+      },
+      {
+        nombre: 'Paula Andrea Taborda Ortiz',
+        cargo: 'Diseñadora Instruccional',
+        centro:
+          'Centro de la Industria, la Empresa y Los Servicios CIES - Regional Norte de Santander',
+      },
+      {
+        nombre: 'Carolina Coca Salazar',
+        cargo: 'Asesora Metodológica',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
+      },
+      {
+        nombre: 'Sandra Patricia Hoyos Sepúlveda',
+        cargo: 'Corrección de estilo',
+        centro: 'Centro de Diseño y Metrología - Regional Distrito Capital',
+      },
     ],
     desarrolloProducto: [
       {
@@ -205,22 +415,21 @@ export default {
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
+        nombre: 'Antonio Vecino Valero',
         cargo: 'Diseño web',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
-        cargo: 'Producción audiovisual',
+        nombre: 'Marcia Elizabeth Solano Alvarez',
+        cargo: 'Desarrollo front-end',
         centro:
           'Centro Industrial del Diseño y la Manufactura - Regional Santander',
       },
       {
-        nombre: 'Nombre',
-        cargo: 'Desarrollo front-end',
-        centro:
-          'Centro Industrial del Diseño y la Manufactura - Regional Santander',
+        nombre: 'Zuleidy María Ruíz Torres',
+        cargo: 'Producción audiovisual',
+        centro: 'Centro de Comercio y Servicios - Regional Tolima',
       },
       {
         nombre: 'Nombre',
@@ -230,16 +439,6 @@ export default {
       },
     ],
     gestoresRepositorio: [
-      {
-        nombre: 'Álvaro Andrés Angarita Ramirez',
-        cargo: 'Validación y vinculación en plataforma LMS',
-        centro: 'Centro de Comercio y Servicios - Regional Tolima',
-      },
-      {
-        nombre: 'Daniel Felipe Varón Molina',
-        cargo: 'Validación y vinculación en plataforma LMS',
-        centro: 'Centro de Comercio y Servicios - Regional Tolima',
-      },
       {
         nombre: 'Milady Tatiana Villamil Castellanos',
         cargo: 'Validación y vinculación en plataforma LMS',
